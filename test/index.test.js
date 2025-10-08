ada70d9c602f3bce151f223cb8f33c00f6020dfb
angelgon214 <angelgonzalezsantafe@gmail.com>
2025-10-07 23:25:15 -0600

    first commit

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
-const index = require('./testindex');
+const hash = require('./testindex.js');
 
-test('Filter even numbers', () => {
+test('Verificar Hash', () => {
     const message = "contraseña";
-    const filteredNumbers = index(message);
-    expect(filteredNumbers).toEqual("559a0612917c8c516c7980c38b376cf2bb2387ae1b6944b3b32245ff4f2bad4d5db3811f02cff394fb62fa3feefd03e2eb83ba7bcca72a912bb87ea609c424da");
+    const hashtest = hash(message);
+    expect(hashtest).toEqual("edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5");
 });
--- test/testindex.js
+++ test/testindex.js
@@ -1,10 +1,9 @@
 import crypto from "crypto";
 
 function hash(mensaje) {
-    const hash = crypto.createHash('sha512');
+    const hash = crypto.createHash('sha256');
     hash.update(mensaje);
     return hash.digest('hex');
 }
-
 
 module.exports = hash;
