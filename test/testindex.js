4ca2aa6198889443f4580fc42a2d6b4395363b4f
angelgon214 <angelgonzalezsantafe@gmail.com>
2025-10-08 20:03:30 -0600

    integración de testindex.js

--- test/testindex.js
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-function hash(mensaje) {
-    const hash = crypto.createHash('sha256');
-    hash.update(mensaje);
-    return hash.digest('hex');
-}
-
-module.exports = hash;
